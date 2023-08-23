141fc88cee4863eafa73c3951fe1e67b02905ef5
Samson Chege <samchege09@gmail.com>
2023-08-23 03:28:02 -0500

    Initial Set Up

--- client/inventory-app/src/App.jsx
+++ client/inventory-app/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Footer from "./components/home/Footer"
+import Navbar from "./components/home/Navbar"
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Box from '@mui/material/Box';
+import Home from "./pages/home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const Layout =()=>{
+    return(
+      <Box>
+      < Navbar/>
+      < Outlet />
+      < Footer/>
+      </Box>
+    )
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+
+
+  return <RouterProvider router={router} />;
 }
 
 export default App
