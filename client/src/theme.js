af774d2d180baff497bd97bb774a3a62d5386e11
Samson Chege <samchege09@gmail.com>
2023-08-23 06:29:42 -0500

    Added Theme

---
+++ client/src/theme.js
@@ -0,0 +1,36 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette:{
+    mode: 'light',
+
+    primary:{
+      main: "#FAFAFA",
+      whole: "#fff",
+      light: "#fff",
+    },
+    secondary:{
+      main: '#15c630',
+    },
+    otherColor:{
+      main:"#999"
+    }
+  },
+
+  typography: {
+    fontFamily:[
+      'Noto Sans JP',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      "Helvetica Neue",
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      "Segoe UI Emoji",
+      "Segoe UI Symbol",
+    ]
+  },
+
+});
