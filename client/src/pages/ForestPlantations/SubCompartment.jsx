87da0e19f3bbfab7e1f25645ca328f90b8cb52bf
nyasakadivinah@gmail.com <nyasakadivinah@gmail.com>
2023-09-07 11:30:52 +0300

    subcompartment

--- client/src/pages/ForestPlantations/SubCompartment.jsx
+++ client/src/pages/ForestPlantations/SubCompartment.jsx
@@ -1,9 +1,12 @@
 import { Box, Card, Container, Paper, Typography } from '@mui/material'
 import { green, red } from '@mui/material/colors'
+import { useForm } from 'react-hook-form';
 import React from 'react'
 
 // function subcompartment() {
 const SubCompartment=()=>{
+    const { register, handleSubmit, watch, formState: { errors } } = useForm({mode: 'onChange'});
+
     return (
         <Container sx={{width:'2000px', bgcolor: 'red'}}>
             <paper elevation={1}> 
@@ -19,8 +22,19 @@ const SubCompartment=()=>{
                 boxShadow: 'none',
                 color: 'red',
                 bgcolor: 'orange'}}>
-                    </Card>
-                    </Box>
+            <form onSubmit={handleSubmit}>
+            <div className='inputForm'>
+            <label>Full Name</label>
+            <input type="text" placeholder='Full Name as it appears on you ID' 
+                {...register("fullname", { 
+                    required: {value: true, message: "Name is Required"},
+                    maxLength: {value: 100, message: "Cannot be Longer than 100 characters"}, 
+                    pattern: {value: /^[A-Za-z]+$/, message: "Cam Only Contain Letters"} })} />
+            
+</div>
+</form>
+                </Card>
+                </Box>
         </Container>
       )
 }
