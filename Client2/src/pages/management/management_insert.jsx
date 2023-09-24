75e6bce975cf6501ae86884cf958447b57e1430d
nyasakadivinah@gmail.com <nyasakadivinah@gmail.com>
2023-09-24 12:55:46 +0300

    divinah

--- Client2/src/pages/management/management_insert.jsx
+++ Client2/src/pages/management/management_insert.jsx
@@ -11,19 +11,18 @@ import styles from "../../styles";
 import { handleError, updateData } from "../../actions/fetchMethods";
 import { useLocation, } from "react-router-dom";
 import SelectInput from "../../components/formComponents/SelectInput";
-import { useEffect } from "react";
-import DisabledNumericalField from "../../components/formComponents/DisabledNumericalField";
 
 const ManagementInsertForm = () => {
 
 const state =useLocation().state
 
+const { register, handleSubmit, watch, reset,  formState: { errors } } = useForm({mode: 'onChange'});
 
-const { register, handleSubmit, watch, reset, setValue, formState: { errors } } = useForm({mode: 'onChange'});
+if(state){
+  reset(state)
+}
 
-      const{ dispatch}= useValue();
-
-      console
+    const{ dispatch}= useValue();
 
 
       const { data: conservanciesData, isLoading: conLoading } = useQuery(
@@ -40,73 +39,45 @@ const { register, handleSubmit, watch, reset, setValue, formState: { errors } } 
         },
       );
       
-  //
+      
 
   //counties 
 
-        const fetchCountiesByConservancy = async ()=>{
-          const res = await newRequest.get(`/county/${watch('conservancy')}`)
-          return res.data
-        }
+  const fetchCountiesByConservancy = async ()=>{
+    const res = await newRequest.get(`/county/${watch('conservancy')}`)
+    return res.data
+  }
 
-        const { data: counties = [] } = useQuery(
-          ['counties', watch('conservancy')],
-          () => fetchCountiesByConservancy(watch('conservancy')),
-          {
-            enabled: !!watch('conservancy'),
-            onError: (error) => {
-              console.error('Error:', error);
-              handleError(dispatch, error);
-            },
-          }
-        );
+  const { data: counties = [] } = useQuery(
+    ['counties', watch('conservancy')],
+    () => fetchCountiesByConservancy(watch('conservancy')),
+    {
+      enabled: !!watch('conservancy'),
+      onError: (error) => {
+        console.error('Error:', error);
+        handleError(dispatch, error);
+      },
+    }
+  );
   
 //station
 
-    const fetchStationByCounty = async ()=>{
-        const res = await newRequest.get(`/station/${watch('county')}`)
-        return res.data
-      }
-
-      const { data: station = [], } = useQuery(['station', watch('county')], () => fetchStationByCounty(watch('county')), {
-            enabled: !!watch('county'),
-            onError: (error) => {
-              console.error('Error:', error);
-              handleError(dispatch, error);
-            },
-            },
-          );
-      //age calculation
-
-      const plantingYear = watch('planting_year'); 
-
-      const calculateAge = (startYear) => {
-        const currentYear = new Date().getFullYear();
-        return currentYear - parseInt(startYear);
-      };
+const fetchStationByCounty = async ()=>{
+    const res = await newRequest.get(`/station/${watch('county')}`)
+    return res.data
+  }
+  const { data: station = [], } = useQuery(['station', watch('county')], () => fetchStationByCounty(watch('county')), {
+        enabled: !!watch('county'),
+        onError: (error) => {
+          console.error('Error:', error);
+          handleError(dispatch, error);
+        },
+        },
+      );
 
       const submit = (data)=>{
-        if(state){
-          console.log("executing update")
-          updateData('patch', `/subcompartment/${state.subcompartment_id}`, data, dispatch)
-        }
-        else{
-          updateData('post', '/subcompartment', data, dispatch)
-        }
+        updateData('post', '/subcompartment', data, dispatch)
       };
-
-      useEffect(()=>{
-        reset(state)
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      },[])
-
-      useEffect(()=>{
-        if(plantingYear){
-          setValue('age', calculateAge(plantingYear), { shouldValidate: true })
-        }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      },[plantingYear])
-
   return (
     <>
     <LoadingBackdrop />
@@ -115,6 +86,58 @@ const { register, handleSubmit, watch, reset, setValue, formState: { errors } } 
 <div className="h-full mx-auto border-4 rounded-2xl bg-white py-5 my-5 w-11/12">
     <form onSubmit={handleSubmit(submit)}>
         <div className="grid grid-cols-12 gap-1">
+         {/* <div className=" col-span-12 sm:col-span-12 md:col-span-4 lg:col-span-4 
+         w-full  justify-center flex-col items-center px-4">
+         <NameInput
+        placeholder='conservancy1'
+        name="conservancy1"
+        label='Conservancy1'
+        maximLength={50}
+        minLength={5}
+        ifRequired={true}
+        errors={errors}
+        register={register}/>
+        </div>
+        <div className=" col-span-12 sm:col-span-12 md:col-span-4 lg:col-span-4 
+         w-full  justify-center flex-col items-center px-4">
+         <NameInput
+        placeholder='county1'
+        name="county1"
+        label='County1                                        '
+        maximLength={50}
+        minLength={5}
+        ifRequired={true}
+        errors={errors}
+        register={register}/>
+        </div>
+        <div className=" col-span-12 sm:col-span-12 md:col-span-4 lg:col-span-4 
+         w-full  justify-center flex-col items-center px-4">
+         <NameInput
+        placeholder='station'
+        name="station"
+        label='Station                                        '
+        maximLength={50}
+        minLength={5}
+        ifRequired={true}
+        errors={errors}
+        register={register}/>
+        </div> */}
+
+{/* 
+        {!conLoading && (<div className=" col-span-12 sm:col-span-12 md:col-span-3 lg:col-span-4 
+         w-full  justify-center flex-col items-center px-4">
+            <label className={`${styles.formLabels}`}>Conservancy</label>
+            <select 
+            {...register('conservancy', {required : true})}
+
+            className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
+                <option>Select Conservancy</option>
+                {
+                    conservanciesData?.map((item, index)=> (
+                        <option key={index} value={item.conservancy_id}>{item.conservancy_name}</option>
+                    ))}
+            </select>
+        </div>)} */}
         {
           !conLoading && (
             <div className="col-span-12 sm:col-span-12 md:col-span-3 lg:col-span-4 
@@ -141,7 +164,6 @@ const { register, handleSubmit, watch, reset, setValue, formState: { errors } } 
             {...register('county', {required : true})}
 
             className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
-                 <option value=''>{'Select County'}</option>
                 {
                     counties.map((item, index)=> (
                         <option key={index} value={item.county_id}>{item.county_name}</option>
@@ -154,7 +176,7 @@ const { register, handleSubmit, watch, reset, setValue, formState: { errors } } 
             <label className={`${styles.formLabels}`}>Forest Station</label>
             <select 
             defaultValue={{ label: "Select Station", value: 'placeholder'}}
-            {...register('forest_station', {required : true})}
+            {...register('station', {required : true})}
 
             className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
                 {
@@ -167,7 +189,7 @@ const { register, handleSubmit, watch, reset, setValue, formState: { errors } } 
          w-full  justify-center flex-col items-center px-4">
          <NameInput
         placeholder='sub_compartment'
-        name="subcompartment"
+        name="sub_compartment"
         label='SubCompartment                                        '
         maximLength={50}
         minLength={5}
@@ -279,20 +301,18 @@ const { register, handleSubmit, watch, reset, setValue, formState: { errors } } 
         errors={errors}
         register={register}/>
         </div>
-        {
-          plantingYear && (
-            <div className=" col-span-12 sm:col-span-12 md:col-span-4 lg:col-span-4 w-full  justify-center flex-col items-center px-4">
-             < DisabledNumericalField 
-             name='age'
-             placeholder='age'
-             ifRequired={true}
-             label="Age (in Years)"
-             errors={errors}
-             register={register}
-             />
-           </div>
-          )
-        }
+        <div className=" col-span-12 sm:col-span-12 md:col-span-4 lg:col-span-4 
+         w-full  justify-center flex-col items-center px-4">
+         <NumericalInput
+        placeholder='age'
+        name="age"
+        label='Age(in years)'
+        max={4000}
+        mi={50}
+        ifRequired={true}
+        errors={errors}
+        register={register}/>
+        </div>
         <div className=" col-span-12 sm:col-span-12 md:col-span-4 lg:col-span-4 
          w-full  justify-center flex-col items-center px-4">
          <NameInput
